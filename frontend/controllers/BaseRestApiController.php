58fd78a2a82275a8a1623197aef1132870f37c62
ihopeso <brnv.ma@gmail.com>
2024-07-28 01:23:25 +1000

    add serializer and contentNegotiator behavior to BaseRestApiController

--- frontend/controllers/BaseRestApiController.php
+++ frontend/controllers/BaseRestApiController.php
@@ -4,7 +4,32 @@ namespace frontend\controllers;
 
 use yii\rest\ActiveController;
 
-final class BaseRestApiController extends ActiveController
+class BaseRestApiController extends ActiveController
 {
+
+    public $serializer = [
+        'class' => 'yii\rest\Serializer',
+        'collectionEnvelope' => 'item',
+    ];
+
+
+    public function checkAccess($action, $model = null, $params = [])
+    {
+        return true;
+    }
+
+    public function behaviors()
+    {
+        return [
+            'contentNegotiatior' => [
+                'class' => \yii\filters\ContentNegotiator::class,
+                'formatParam' => 'format',
+                'formats' => [
+                    'application/json' => \yii\web\Response::FORMAT_JSON,
+                    'xml' => \yii\web\Response::FORMAT_XML,
+                ],
+            ],
+        ];
+    }
 
 }
