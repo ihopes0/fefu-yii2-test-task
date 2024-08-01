090d498c7250aef7760b08a1608a2b2bf1a9a537
ihopeso <brnv.ma@gmail.com>
2024-08-01 17:35:16 +1000

    move same logic of scheduling in the console and via the rest api to a separate class 'backend\commands\Scheduler'

--- console/controllers/ScheduleController.php
+++ console/controllers/ScheduleController.php
@@ -3,8 +3,8 @@
 namespace console\controllers;
 
 use frontend\models\Meetup;
-use frontend\models\User;
 use frontend\models\UserMeetup;
+use backend\commands\Scheduler;
 use yii\console\Controller;
 
 /**
@@ -14,82 +14,20 @@ final class ScheduleController extends Controller
 {
 
     /**
-     * Creates a schedule for given users using a simple approximate greedy algorithm.
+     * Creates a schedule via Scheduler class.
      * @return int 0 if successful
      * @throws \Exception on any error
      */
-    public function actionCreateFor(array $usersId, string $date): int
+    public function actionCreateFor(string $usersId, string $date): int
     {
-        if (!strtotime($date)) {
-            throw new \Exception('Date validation error: ' . '"' . $date . '"' . " is not a valid date");
-        }
-        $date = date('y-m-d', strtotime($date));
+        echo "Making schedule for Users " . $usersId . " on {$date}\n";
 
-        if ($usersId[0] == 'all') {
-            $users = User::find()->asArray()->all();
-        } else {
-            $users = User::find()
-                ->where(['id' => $usersId])
-                ->asArray()
-                ->all();
-        }
-        if (!$users) {
-            throw new \Exception('User not found');
-        }
-
-        $meetups = Meetup::find()
-            ->where(['>=', 'starts_at', strtotime("{$date} 00:00")])
-            ->andWhere(['<=', 'starts_at', strtotime("{$date} 23:59")])
-            ->orderBy(['ends_at' => SORT_ASC])
-            ->asArray()
-            ->all();
-        if (!$meetups) {
-            echo "There are no meetings on {$date}\n";
-            return 1;
-        }
-
-        echo "Making schedule for Users " . implode(',', $usersId) . " on {$date}\n";
-
-        $this->makeSchedule($users, $meetups);
+        Scheduler::make($usersId, $date);
 
         echo "Success!\n";
 
         return 0;
     }
-
-
-    private function makeSchedule(array $users, array $meetups): void
-    {
-        foreach ($users as $user) {
-            $scheduledMeetups = [];
-            $scheduledMeetupsId = [];
-            $lastEndTime = 0;
-
-            foreach ($meetups as $index => $meetup) {
-                if (
-                    $meetup['starts_at'] >= $lastEndTime &&
-                    $meetup['count_participated_members'] < $meetup['max_number_of_members']
-                ) {
-                    $scheduledMeetups[] = $meetup;
-                    $lastEndTime = $meetup['ends_at'];
-
-                    $meetups[$index]['count_participated_members'] += 1;
-                    $scheduledMeetupsId[] = $meetup['id'];
-                }
-            }
-
-            foreach ($scheduledMeetups as $scheduledMeetup) {
-                $userMeetup = new UserMeetup();
-                $userMeetup->user_id = $user['id'];
-                $userMeetup->meetup_id = $scheduledMeetup['id'];
-                if (!$userMeetup->save()) {
-                    throw new \Exception('Failed to save meetup for user: ' . json_encode($userMeetup->errors));
-                }
-            }
-            Meetup::updateAllCounters(['count_participated_members' => 1], ['id' => $scheduledMeetupsId]);
-        }
-    }
-
 
     // for development only
     public function actionDeleteAllScheduledMeetups()
--- frontend/controllers/UserController.php
+++ frontend/controllers/UserController.php
@@ -2,6 +2,7 @@
 
 namespace frontend\controllers;
 
+use backend\commands\Scheduler;
 use frontend\models\Meetup;
 use frontend\models\User;
 use frontend\models\UserMeetup;
@@ -13,74 +14,10 @@ final class UserController extends BaseRestApiController
 
     public function actionCreateSchedule(string $usersId, string $date)
     {
-        $usersId = explode(',', $usersId);
-
-        if (!strtotime($date)) {
-            throw new \Exception('Date validation error: ' . '"' . $date . '"' . " is not a valid date");
-        }
-        $date = date('y-m-d', strtotime($date));
-
-        if ($usersId[0] == 'all') {
-            $users = User::find()->asArray()->all();
-        } else {
-            $users = User::find()
-                ->where(['id' => $usersId])
-                ->asArray()
-                ->all();
-        }
-        if (!$users) {
-            throw new \Exception('User not found');
-        }
-
-        $meetups = Meetup::find()
-            ->where(['>=', 'starts_at', strtotime("{$date} 00:00")])
-            ->andWhere(['<=', 'starts_at', strtotime("{$date} 23:59")])
-            ->orderBy(['ends_at' => SORT_ASC])
-            ->asArray()
-            ->all();
-        if (!$meetups) {
-            return "There are no meetings on {$date}\n";
-        }
-
-        $this->makeSchedule($users, $meetups);
+        Scheduler::make($usersId, $date);
 
         Yii::$app->response->statusCode = 200;
         Yii::$app->response->content = "OK";
         return Yii::$app->response->send();
-    }
-
-
-    private function makeSchedule(array $users, array $meetups): void
-    {
-        foreach ($users as $user) {
-            $scheduledMeetups = [];
-            $lastEndTime = 0;
-
-            foreach ($meetups as $index => $meetup) {
-                if (
-                    $meetup['starts_at'] >= $lastEndTime &&
-                    $meetup['count_participated_members'] < $meetup['max_number_of_members']
-                ) {
-                    $scheduledMeetups[] = $meetup;
-                    $lastEndTime = $meetup['ends_at'];
-
-                    $meetups[$index]['count_participated_members'] += 1;
-                }
-            }
-
-            $savedMeetupsId = [];
-
-            foreach ($scheduledMeetups as $scheduledMeetup) {
-                $userMeetup = new UserMeetup();
-                $userMeetup->user_id = $user['id'];
-                $userMeetup->meetup_id = $scheduledMeetup['id'];
-                if (!$userMeetup->save()) {
-                    echo 'Failed to save meetup for user: ' . json_encode($userMeetup->errors) . "\n";
-                    continue;
-                }
-                $savedMeetupsId[] = $scheduledMeetup['id'];
-            }
-            Meetup::updateAllCounters(['count_participated_members' => 1], ['id' => $savedMeetupsId]);
-        }
     }
 }
