72c2e4fb52e8fe7721efc2a41ef38d44f2ecb240
ihopeso <brnv.ma@gmail.com>
2024-08-09 00:06:01 +1000

    удалена авторизация, добавлена инструкция к использованию на основную страницу

--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -28,35 +28,23 @@ AppAsset::register($this);
 <header>
     <?php
     NavBar::begin([
-        'brandLabel' => Yii::$app->name,
+        'brandLabel' => '',
         'brandUrl' => Yii::$app->homeUrl,
         'options' => [
             'class' => 'navbar navbar-expand-md navbar-dark bg-dark fixed-top',
         ],
     ]);
     $menuItems = [
-        ['label' => 'Home', 'url' => ['/site/index']],
-        ['label' => 'About', 'url' => ['/site/about']],
-        ['label' => 'Contact', 'url' => ['/site/contact']],
+        ['label' => 'Users', 'url' => ['/user']],
+        ['label' => 'Meetings', 'url' => ['/meetup']],
+        ['label' => 'Users with schedule', 'url' => ['/user?expand=meetups']],
+        ['label' => 'Meetings with participants', 'url' => ['/meetup?expand=users']],
     ];
-    if (Yii::$app->user->isGuest) {
-        $menuItems[] = ['label' => 'Signup', 'url' => ['/site/signup']];
-    }
 
     echo Nav::widget([
         'options' => ['class' => 'navbar-nav me-auto mb-2 mb-md-0'],
         'items' => $menuItems,
     ]);
-    if (Yii::$app->user->isGuest) {
-        echo Html::tag('div',Html::a('Login',['/site/login'],['class' => ['btn btn-link login text-decoration-none']]),['class' => ['d-flex']]);
-    } else {
-        echo Html::beginForm(['/site/logout'], 'post', ['class' => 'd-flex'])
-            . Html::submitButton(
-                'Logout (' . Yii::$app->user->identity->username . ')',
-                ['class' => 'btn btn-link logout text-decoration-none']
-            )
-            . Html::endForm();
-    }
     NavBar::end();
     ?>
 </header>
--- frontend/views/site/index.php
+++ frontend/views/site/index.php
@@ -2,14 +2,13 @@
 
 /** @var yii\web\View $this */
 
-$this->title = 'My Yii Application';
+$this->title = 'Главная';
 ?>
 <div class="site-index">
     <div class="p-5 mb-4 bg-transparent rounded-3">
         <div class="container-fluid py-5 text-center">
-            <h1 class="display-4">Congratulations!</h1>
-            <p class="fs-5 fw-light">You have successfully created your Yii-powered application.</p>
-            <p><a class="btn btn-lg btn-success" href="https://www.yiiframework.com">Get started with Yii</a></p>
+            <h1 class="display-4">Тестовое задание по Yii2 - fefu</h1>
+            <p class="fs-5 fw-light"></p>
         </div>
     </div>
 
@@ -17,34 +16,36 @@ $this->title = 'My Yii Application';
 
         <div class="row">
             <div class="col-lg-4">
-                <h2>Heading</h2>
+                <h4>Чтобы посмотреть пользователей</h4>
 
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
+                <p>/user</p>
 
-                <p><a class="btn btn-outline-secondary" href="https://www.yiiframework.com/doc/">Yii Documentation &raquo;</a></p>
+                <h4>с расписанием</h4>
+
+                <p>/user?expand=meetups</p>
+
             </div>
             <div class="col-lg-4">
-                <h2>Heading</h2>
+                <h4>Чтобы посмотреть встречи</h4>
 
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
+                <p>/meetup</p>
 
-                <p><a class="btn btn-outline-secondary" href="https://www.yiiframework.com/forum/">Yii Forum &raquo;</a></p>
+                <h4>с участниками</h4>
+
+                <p>/meetup?expand=users</p>
+
             </div>
             <div class="col-lg-4">
-                <h2>Heading</h2>
+                <h4>Чтобы составить расписание пользователю</h4>
 
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
+                <p>/user/create-schedule?id=&day=</p>
+                <p>
+                    <span>где id - id пользователя или пользователей (через запятую), для всех пользователей id=all;</span>
+                    <br>
+                    <span>day - дата, на которую нужно составить расписание, формат y-m-d</span>
 
-                <p><a class="btn btn-outline-secondary" href="https://www.yiiframework.com/extensions/">Yii Extensions &raquo;</a></p>
+                </p>
+
             </div>
         </div>
 
